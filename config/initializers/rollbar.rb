44ab8ced721360f0a6276697c884288b8a966d87
Ross Dakin <rossdakin@gmail.com>
2014-11-18 04:06:34 -0800

    Use client/server-specific Rollbar access tokens

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -3,7 +3,7 @@ Rollbar.configure do |config|
   # Without configuration, Rollbar is enabled in all environments.
   # To disable in specific environments, set config.enabled=false.
 
-  config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
+  config.access_token = ENV['ROLLBAR_SERVER_ACCESS_TOKEN']
 
   # Here we'll disable in 'test':
   if Rails.env.test?
