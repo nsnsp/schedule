dd16924de9794c2e5d5359cf98ebc1017e1ea63d
Ross Dakin <rossdakin@gmail.com>
2014-12-21 20:58:01 -0800

    Log SQL in dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,6 +19,9 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
+  # Log SQL
+  ActiveRecord::Base.logger = Logger.new(STDOUT)
+
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
