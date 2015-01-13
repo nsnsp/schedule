2c59c777f28fc28aea1e7b70d11bce919a7d047b
Ross Dakin <rossdakin@gmail.com>
2015-01-12 20:20:25 -0800

    Add new commitment type (PatrolAuxiliary)

--- app/models/commitments.rb
+++ app/models/commitments.rb
@@ -12,6 +12,12 @@ module Commitments
     def self.day_multiplier() 0.5 end
   end
 
+  class PatrolAuxiliary < Commitment
+    def self.display_color() '#D9534F' end
+    def self.display_text() 'Patrol (MMHQ)' end
+    def self.bootstrap_class_suffix() 'danger' end
+  end
+
   class Instructor < Commitment
     def self.display_color() '#5CB85C' end
     def self.display_verb() 'Instruct' end
@@ -31,5 +37,6 @@ module Commitments
     def self.bootstrap_class_suffix() 'info' end
   end
 
-  DISPLAY_ORDER = [Other, Patrol, PatrolPM, Instructor, Candidate]
+  DISPLAY_ORDER = [Other, Patrol, PatrolPM, PatrolAuxiliary, Instructor,
+                   Candidate]
 end
