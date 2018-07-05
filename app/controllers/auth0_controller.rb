9cce2977b095410350fee3c6c4642acf33c2954b
Ross Dakin <rossdakin@gmail.com>
2018-07-05 02:21:33 -0400

    Set logout redirect URL in ENV var

--- app/controllers/auth0_controller.rb
+++ app/controllers/auth0_controller.rb
@@ -55,7 +55,7 @@ class Auth0Controller < ApplicationController
     uri = URI::HTTPS.build(host: ENV['AUTH0_DOMAIN'], path: "/v2/logout")
     uri.query = {
       client_id: ENV['AUTH0_CLIENT_ID'],
-      returnTo: root_url
+      returnTo: ENV['AUTH0_LOGOUT_URL']
     }.to_query
 
     redirect_to uri.to_s
