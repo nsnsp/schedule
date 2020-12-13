9a2d539912e3c7e06b7dcfd5eb726fa72a3ba9e2
Ross Dakin <rossdakin@gmail.com>
2020-12-13 14:26:04 -0800

    Use sendgrid API key rather than username/password

--- config/initializers/sendgrid.rb
+++ config/initializers/sendgrid.rb
@@ -2,9 +2,9 @@ Rails.application.config.action_mailer.default_options = {
   from: 'support@nsnsp.org'
 }
 Rails.application.config.action_mailer.smtp_settings = {
-  user_name: ENV['SENDGRID_USERNAME'],
-  password: ENV['SENDGRID_PASSWORD'],
-  domain: 'nsnsp.org',
+  user_name: 'apikey',
+  password: ENV['SENDGRID_API_KEY'],
+  domain: 'email.nsnsp.org',
   address: 'smtp.sendgrid.net',
   port: 587,
   authentication: :plain,
