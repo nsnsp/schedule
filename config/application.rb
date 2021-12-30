2b82a0dd9854b64193692520a45d59287c8e8cb9
Ross Dakin <rossdakin@gmail.com>
2021-12-30 16:48:18 -0500

    Rails 4.2 to 5.0 configuration
    rails app:update, followed by some tweaks

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
 require 'rails/all'
 
@@ -16,9 +16,5 @@ module Nsnsp
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
     config.time_zone = 'Pacific Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
   end
 end
--- config/spring.rb
+++ config/spring.rb
@@ -1,1 +1,6 @@
-Spring.watch '.env'
+%w(
+  .ruby-version
+  .rbenv-vars
+  tmp/restart.txt
+  tmp/caching-dev.txt
+).each { |path| Spring.watch(path) }
