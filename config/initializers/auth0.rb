b1a3ba54750e833df44a277fe503e6013c7acd3b
Ross Dakin <rossdakin@gmail.com>
2021-12-31 02:25:46 -0500

    Auth0

--- config/initializers/auth0.rb
+++ config/initializers/auth0.rb
@@ -5,16 +5,3 @@ Rails.application.config.middleware.use OmniAuth::Builder do
            ENV['AUTH0_DOMAIN'],
            callback_path: "/auth/auth0/callback")
 end
-
-OmniAuth.config.on_failure = Proc.new { |env|
-  message_key = env['omniauth.error.type']
-  error_code = Rack::Utils.escape(env['omniauth.error'].code)
-  error_description = Rack::Utils.escape(env['omniauth.error'].description)
-
-  new_path = ("#{env['SCRIPT_NAME']}#{OmniAuth.config.path_prefix}/failure" \
-              "?message=#{message_key}" \
-              "&error_code=#{error_code}" \
-              "&error_description=#{error_description}")
-
-  Rack::Response.new(['302 Moved'], 302, 'Location' => new_path).finish
-}
