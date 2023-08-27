ba6ef27bc5aa989bb911751f792357305cfa1119
Ross Dakin <rossdakin@users.noreply.github.com>
2023-08-27 04:37:09 +0000

    bin/rails app:update

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,21 +11,20 @@ module Nsnsp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Pacific Time (US & Canada)'
+
     # Setting this baesd on what I think the docs are telling me to do:
     # https://guides.rubyonrails.org/upgrading_ruby_on_rails.html#autoloading
     config.autoloader = :zeitwerk
 
     # https://guides.rubyonrails.org/upgrading_ruby_on_rails.html#config-add-autoload-paths-to-load-path
     config.add_autoload_paths_to_load_path = false
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-    config.time_zone = 'Pacific Time (US & Canada)'
   end
 end
