8e0b18cf987a636b95e4c49ae4d72e2900abaefa
Ross Dakin <rossdakin@users.noreply.github.com>
2023-08-27 05:06:29 +0000

    rake db:schema:dump

--- db/schema.rb
+++ db/schema.rb
@@ -1,78 +1,73 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[6.1].define(version: 20150111061549) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_08_27_044334) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
 
-  create_table "commitments", force: :cascade do |t|
-    t.integer  "user_id"
-    t.date     "date",                   null: false
-    t.string   "type",       limit: 255, null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "uuid"
+  create_table "commitments", id: :serial, force: :cascade do |t|
+    t.integer "user_id"
+    t.date "date", null: false
+    t.string "type", limit: 255, null: false
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
+    t.string "uuid"
+    t.index ["date"], name: "index_commitments_on_date"
+    t.index ["type"], name: "index_commitments_on_type"
+    t.index ["user_id"], name: "index_commitments_on_user_id"
+    t.index ["uuid"], name: "index_commitments_on_uuid", unique: true
   end
 
-  add_index "commitments", ["date"], name: "index_commitments_on_date", using: :btree
-  add_index "commitments", ["type"], name: "index_commitments_on_type", using: :btree
-  add_index "commitments", ["user_id"], name: "index_commitments_on_user_id", using: :btree
-  add_index "commitments", ["uuid"], name: "index_commitments_on_uuid", unique: true, using: :btree
-
-  create_table "identities", force: :cascade do |t|
-    t.integer  "user_id"
-    t.string   "auth0_uid",      limit: 255, null: false
-    t.string   "first_name",     limit: 255
-    t.string   "last_name",      limit: 255
-    t.string   "email",          limit: 255
-    t.text     "image_url"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.boolean  "email_verified"
+  create_table "identities", id: :serial, force: :cascade do |t|
+    t.integer "user_id"
+    t.string "auth0_uid", limit: 255, null: false
+    t.string "first_name", limit: 255
+    t.string "last_name", limit: 255
+    t.string "email", limit: 255
+    t.text "image_url"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
+    t.boolean "email_verified"
+    t.index ["auth0_uid"], name: "index_identities_on_auth0_uid", unique: true
+    t.index ["user_id"], name: "index_identities_on_user_id"
   end
 
-  add_index "identities", ["auth0_uid"], name: "index_identities_on_auth0_uid", unique: true, using: :btree
-  add_index "identities", ["user_id"], name: "index_identities_on_user_id", using: :btree
-
-  create_table "users", force: :cascade do |t|
-    t.string   "first_name",                  limit: 255
-    t.string   "last_name",                   limit: 255
-    t.string   "email",                       limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.boolean  "suspended",                               default: false, null: false
-    t.integer  "roles_mask"
-    t.string   "phone"
-    t.string   "auth_token"
-    t.boolean  "daily_schedule_notification",             default: false
-    t.boolean  "early_schedule_notification",             default: false
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "first_name", limit: 255
+    t.string "last_name", limit: 255
+    t.string "email", limit: 255
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
+    t.boolean "suspended", default: false, null: false
+    t.integer "roles_mask"
+    t.string "phone"
+    t.string "auth_token"
+    t.boolean "daily_schedule_notification", default: false
+    t.boolean "early_schedule_notification", default: false
+    t.index ["daily_schedule_notification"], name: "index_users_on_daily_schedule_notification"
+    t.index ["early_schedule_notification"], name: "index_users_on_early_schedule_notification"
+    t.index ["suspended"], name: "index_users_on_suspended"
   end
 
-  add_index "users", ["daily_schedule_notification"], name: "index_users_on_daily_schedule_notification", using: :btree
-  add_index "users", ["early_schedule_notification"], name: "index_users_on_early_schedule_notification", using: :btree
-  add_index "users", ["suspended"], name: "index_users_on_suspended", using: :btree
-
-  create_table "versions", force: :cascade do |t|
-    t.string   "item_type",      limit: 255, null: false
-    t.integer  "item_id",                    null: false
-    t.string   "event",          limit: 255, null: false
-    t.string   "whodunnit",      limit: 255
-    t.text     "object"
-    t.datetime "created_at"
-    t.text     "object_changes"
+  create_table "versions", id: :serial, force: :cascade do |t|
+    t.string "item_type", limit: 255, null: false
+    t.integer "item_id", null: false
+    t.string "event", limit: 255, null: false
+    t.string "whodunnit", limit: 255
+    t.text "object"
+    t.datetime "created_at", precision: nil
+    t.text "object_changes"
+    t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
-
-  add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id", using: :btree
 
 end
