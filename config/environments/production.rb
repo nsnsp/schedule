b79aa5bc0286146680ddc9b436ae5dc0a1d433a5
Ross Dakin <rossdakin@users.noreply.github.com>
2026-08-04 01:16:29 -0400

    Switch cache store from Memcached to Redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  if ENV["MEMCACHEDCLOUD_SERVERS"]
-    config.cache_store = :mem_cache_store, ENV["MEMCACHEDCLOUD_SERVERS"].split(','), { :username => ENV["MEMCACHEDCLOUD_USERNAME"], :password => ENV["MEMCACHEDCLOUD_PASSWORD"] }
+  if ENV["REDISCLOUD_URL"]
+    config.cache_store = :redis_cache_store, { url: ENV["REDISCLOUD_URL"] }
   end
 end
