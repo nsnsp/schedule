91b9ed8ffe8bc708981f2fa9a73e7cccf5539bb1
Ross Dakin <rossdakin@users.noreply.github.com>
2026-08-04 06:20:45 +0000

    Update Rails 7.2.3.2 to 8.0.5.1

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module Nsnsp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -19,8 +24,5 @@ module Nsnsp
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.time_zone = 'Pacific Time (US & Canada)'
-
-    # https://guides.rubyonrails.org/upgrading_ruby_on_rails.html#config-add-autoload-paths-to-load-path
-    config.add_autoload_paths_to_load_path = false
   end
 end
